d599872260d6dede5683a194db21f03337aa9132
Mauricio Rojas <mrojasb2000@gmail.com>
2025-06-20 16:51:04 -0400

    chore: add constructor and functions implements

--- Struct/src/main.rs
+++ Struct/src/main.rs
@@ -5,36 +5,44 @@ enum Friend {
 }
 
 #[derive(Debug)]
+#[allow(dead_code)]
 struct Human {
     name: String,
     age: i8,
-    current_thought: String,
+    current_thought: Option<String>,
     friend: Friend,
 }
 
-fn main() {
-    let another_developer = Human {
-        name: "John Doe".to_string(),
-        age: 25,
-        current_thought: "I need to code!!".to_string(),
-        friend: Friend::NIL,
-    };
+impl Human {
+    fn new(name: &str, age: i8) -> Human {
+        return Human {
+            name: name.to_string(),
+            age,
+            current_thought: None,
+            friend: Friend::NIL,
+        };
+    }
+    fn with_thought(mut self, thought: &str) -> Human {
+        self.current_thought = Some(thought.to_string());
+        return self;
+    }
+    fn with_friend(mut self, friend: Box<Human>) -> Human {
+        self.friend = Friend::HUMAN(friend);
+        return self;
+    }
+}
 
-    let developer = Human {
-        name: "Maxwell Flitton".to_string(),
-        age: 32,
-        current_thought: "nothing".to_string(),
-        friend: Friend::HUMAN(Box::new(another_developer)),
-    };
+fn main() {
+    let another_developer = Human::new("John Doe", 25);
+
+    let developer = Human::new("Maxwell Flitton", 32)
+        .with_friend(Box::new(another_developer))
+        .with_thought("I love Rust!");
+
     match &developer.friend {
         Friend::HUMAN(data) => {
             println!("{}", data.name);
         }
         Friend::NIL => {}
     }
-
-    println!("{:?}", developer);
-    println!("{}", developer.name);
-    println!("{}", developer.age);
-    println!("{}", developer.current_thought);
 }
