ef520048bda9e82fd6ccd0970c7e73b04cc6f70c
Mauricio Rojas <mrojasb2000@gmail.com>
2025-06-20 16:19:57 -0400

    chore: add enums and struct tree

--- Struct/src/main.rs
+++ Struct/src/main.rs
@@ -1,16 +1,38 @@
 #[derive(Debug)]
-struct Human<'a> {
-    name: &'a str,
+enum Friend {
+    HUMAN(Box<Human>),
+    NIL,
+}
+
+#[derive(Debug)]
+struct Human {
+    name: String,
     age: i8,
-    current_thought: &'a str,
+    current_thought: String,
+    friend: Friend,
 }
 
 fn main() {
-    let developer = Human {
-        name: "Maxwell Flitton",
-        age: 32,
-        current_thought: "nothing",
+    let another_developer = Human {
+        name: "John Doe".to_string(),
+        age: 25,
+        current_thought: "I need to code!!".to_string(),
+        friend: Friend::NIL,
     };
+
+    let developer = Human {
+        name: "Maxwell Flitton".to_string(),
+        age: 32,
+        current_thought: "nothing".to_string(),
+        friend: Friend::HUMAN(Box::new(another_developer)),
+    };
+    match &developer.friend {
+        Friend::HUMAN(data) => {
+            println!("{}", data.name);
+        }
+        Friend::NIL => {}
+    }
+
     println!("{:?}", developer);
     println!("{}", developer.name);
     println!("{}", developer.age);
