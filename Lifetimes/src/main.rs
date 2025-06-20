a5df0816647c868f5489236a8aaf935b399ad838
Mauricio Rojas <mrojasb2000@gmail.com>
2025-06-20 15:03:23 -0400

    add variables and function lifetimes

--- Lifetimes/src/main.rs
+++ Lifetimes/src/main.rs
@@ -1,8 +1,41 @@
+// fn get_highest<'a>(first_number: &'a i8, second_number: &'a i8) -> &'a i8 {
+//     if first_number > second_number {
+//         first_number
+//     } else {
+//         second_number
+//     }
+// }
+
+fn filter<'a, 'b>(first_number: &'a i8, second_number: &'b i8) -> &'a i8 {
+    if first_number > second_number {
+        &0
+    } else {
+        first_number
+    }
+}
+
 fn main() {
-    let one: &i8;
+    // let one: &i8;
+    // {
+    //     let two: i8 = 2;
+    //     one = &two;
+    // }
+    // println!("r: {}", one);
+    //
+
+    // let one: i8 = 1;
+    // let outcome: &i8;
+    // {
+    //     let two: i8 = 2;
+    //     let outcome: &i8 = get_highest(&one, &two);
+    // }
+    // println!("outcome: {}", outcome);
+    //
+    let one: i8 = 1;
+    let outcome: &i8;
     {
         let two: i8 = 2;
-        one = &two;
+        outcome = filter(&one, &two);
     }
-    println!("r: {}", one);
+    println!("outcome: {}", outcome);
 }
