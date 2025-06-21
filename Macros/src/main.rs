a9f4ecf3bb294ea13ababbb087ded84978508976
Mauricio Rojas <mrojasb2000@gmail.com>
2025-06-21 15:52:07 -0400

    chore: add metaprogramming with generics

---
+++ Macros/src/main.rs
@@ -0,0 +1,10 @@
+struct Coordinate<T> {
+    x: T,
+    y: T,
+}
+
+fn main() {
+    let one = Coordinate { x: 50, y: 50 };
+    let two = Coordinate { x: 500, y: 500 };
+    let three = Coordinate { x: 5.6, y: 5.6 };
+}
