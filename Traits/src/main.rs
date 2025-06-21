85fe19ca24c0169f207252a2f55c137aec8e73cf
Mauricio Rojas <mrojasb2000@gmail.com>
2025-06-21 15:21:59 -0400

    chore: add implement traits

---
+++ Traits/src/main.rs
@@ -0,0 +1,65 @@
+#[allow(dead_code)]
+struct AdminUser {
+    username: String,
+    password: String,
+}
+#[allow(dead_code)]
+struct User {
+    username: String,
+    password: String,
+}
+
+trait CanEdit {
+    fn edit(&self) {
+        println!("admin is editing");
+    }
+}
+
+trait CanCreate {
+    fn create(&self) {
+        println!("admin is creating");
+    }
+}
+
+trait CanDelete {
+    fn delete(&self) {
+        println!("admin is deleting");
+    }
+}
+
+impl CanDelete for AdminUser {}
+impl CanCreate for AdminUser {}
+impl CanEdit for AdminUser {}
+
+impl CanEdit for User {
+    fn edit(&self) {
+        println!("A standard user {} is editing", self.username);
+    }
+}
+
+fn create<T: CanCreate>(user: &T) -> () {
+    user.create();
+}
+
+fn delete<T: CanDelete>(user: &T) -> () {
+    user.delete();
+}
+
+fn edit<T: CanEdit>(user: &T) -> () {
+    user.edit();
+}
+
+fn main() {
+    let admin = AdminUser {
+        username: "admin".to_string(),
+        password: "password".to_string(),
+    };
+    let user = User {
+        username: "user".to_string(),
+        password: "password".to_string(),
+    };
+    create(&admin);
+    edit(&admin);
+    edit(&user);
+    delete(&admin);
+}
