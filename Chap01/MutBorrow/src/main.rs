b7eb81bd88142e570f1bc64545f414fbcb66bed9
Mauricio Rojas <ext_mauricio.rojasb@tapp.cl>
2025-06-23 11:36:16 -0400

    chore: comments code

--- Chap01/MutBorrow/src/main.rs
+++ Chap01/MutBorrow/src/main.rs
@@ -1,11 +1,11 @@
-fn print(value: &mut i8, value_two: &mut i8) {
-    value += 1;
-    println!("In function the value is: {}", value);
-    value_two += 1;
-}
+// fn print(value: &mut i8, value_two: &mut i8) {
+//     value += 1;
+//     println!("In function the value is: {}", value);
+//     value_two += 1;
+// }
 
 fn main() {
     let mut one: i8 = 5;
-    print(&mut one, &mut one);
+    //print(&mut one, &mut one);
     println!("In main the value is: {}", one);
 }
