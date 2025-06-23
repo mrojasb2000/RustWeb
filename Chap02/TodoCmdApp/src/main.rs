c11a13066bb13f05df48fb710064c3b1684bc855
Mauricio Rojas <ext_mauricio.rojasb@tapp.cl>
2025-06-23 17:17:13 -0400

    chore: add process arguments command line application

--- Chap02/TodoCmdApp/src/main.rs
+++ Chap02/TodoCmdApp/src/main.rs
@@ -1,5 +1,15 @@
 use rand::prelude::*;
+use std::env;
 
+/// This function generates a float number using a number
+/// generator passed into the function.
+///
+/// # Arguments
+/// * generator (&mut ThreadRng): the random number
+/// genarator to generate the random number
+///
+/// # Returns
+/// (f64): random number between 0 -> 10
 fn generate_float(generator: &mut ThreadRng) -> f64 {
     let placeholder: f64 = generator.random();
     return placeholder * 10.0;
@@ -9,4 +19,16 @@ fn main() {
     let mut rng: ThreadRng = rand::rng();
     let random_number = generate_float(&mut rng);
     println!("{}", random_number);
+
+    let args: Vec<String> = env::args().collect();
+    let path: &str = &args[0];
+    println!("{:?}", args);
+
+    if path.contains("/debug/") {
+        println!("Debug is running");
+    } else if path.contains("/release/") {
+        println!("release is running");
+    } else {
+        panic!("The setting is neither debug nor release");
+    }
 }
