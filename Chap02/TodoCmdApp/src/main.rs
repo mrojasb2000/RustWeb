755c7c4d367ca52a473ebb2253585cfea185d024
Mauricio Rojas <ext_mauricio.rojasb@tapp.cl>
2025-06-23 11:56:08 -0400

    chore: add rand dependency and use random generator

--- Chap02/TodoCmdApp/src/main.rs
+++ Chap02/TodoCmdApp/src/main.rs
@@ -1,3 +1,12 @@
+use rand::prelude::*;
+
+fn generate_float(generator: &mut ThreadRng) -> f64 {
+    let placeholder: f64 = generator.random();
+    return placeholder * 10.0;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng: ThreadRng = rand::rng();
+    let random_number = generate_float(&mut rng);
+    println!("{}", random_number);
 }
