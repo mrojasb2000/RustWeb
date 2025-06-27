439f5ec691911341dfbc4ee2ba524f6beaf4d9b4
Mauricio Rojas <ext_mauricio.rojasb@tapp.cl>
2025-06-27 17:09:33 -0400

    chore: improve managing structs with factories

--- Chap02/TodoCmdApp/src/main.rs
+++ Chap02/TodoCmdApp/src/main.rs
@@ -1,13 +1,18 @@
 mod to_do;
-use to_do::structs::done::Done;
-use to_do::structs::pending::Pending;
+use to_do::ItemTypes;
+use to_do::enums::TaskStatus;
+use to_do::to_do_factory;
 
 fn main() {
-    let done = Done::new("shopping");
-    println!("{}", done.super_struct.title);
-    println!("{}", done.super_struct.status.stringify());
-
-    let pending = Pending::new("laundry");
-    println!("{}", pending.super_struct.title);
-    println!("{}", pending.super_struct.status.stringify());
+    let to_do_item = to_do_factory("washing", TaskStatus::DONE);
+    match to_do_item {
+        ItemTypes::Done(item) => {
+            println!("{}", item.super_struct.title);
+            println!("{}", item.super_struct.status.stringify());
+        }
+        ItemTypes::Pending(item) => {
+            println!("{}", item.super_struct.title);
+            println!("{}", item.super_struct.status.stringify());
+        }
+    }
 }
