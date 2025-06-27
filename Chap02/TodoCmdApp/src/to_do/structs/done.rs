94af069ae6b42e8e0e84595a841272c0116b635c
Mauricio Rojas <ext_mauricio.rojasb@tapp.cl>
2025-06-27 18:10:06 -0400

    chore: improve functionality with traits

--- Chap02/TodoCmdApp/src/to_do/structs/done.rs
+++ Chap02/TodoCmdApp/src/to_do/structs/done.rs
@@ -1,9 +1,16 @@
 use super::super::enums::TaskStatus;
+use super::super::traits::delete::Delete;
+use super::super::traits::edit::Edit;
+use super::super::traits::get::Get;
 use super::base::Base;
 
 pub struct Done {
     pub super_struct: Base,
 }
+
+impl Get for Done {}
+impl Edit for Done {}
+impl Delete for Done {}
 
 impl Done {
     pub fn new(input_title: &str) -> Self {
--- Chap02/TodoCmdApp/src/to_do/structs/pending.rs
+++ Chap02/TodoCmdApp/src/to_do/structs/pending.rs
@@ -1,9 +1,16 @@
 use super::super::enums::TaskStatus;
+use super::super::traits::create::Create;
+use super::super::traits::edit::Edit;
+use super::super::traits::get::Get;
 use super::base::Base;
 
 pub struct Pending {
     pub super_struct: Base,
 }
+
+impl Get for Pending {}
+impl Edit for Pending {}
+impl Create for Pending {}
 
 impl Pending {
     pub fn new(input_title: &str) -> Self {
