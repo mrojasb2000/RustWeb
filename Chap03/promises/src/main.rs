638fba246e88898dfe63d2b07e2b0cff278741e9
Mauricio Rojas <ext_mauricio.rojasb@tapp.cl>
2025-08-21 11:50:53 -0400

    chore: add async-std vector tasks on same thread

--- Chap03/promises/src/main.rs
+++ Chap03/promises/src/main.rs
@@ -1,4 +1,7 @@
+use async_std;
 use futures::executor::block_on;
+use futures::future::join_all;
+use std::vec::Vec;
 use std::{thread, time};
 
 async fn do_something(number: i8) -> i8 {
@@ -10,12 +13,24 @@ async fn do_something(number: i8) -> i8 {
 
 fn main() {
     let now = time::Instant::now();
-    let future_three = async {
-        let outcome_one = do_something(2).await;
-        let outcome_two = do_something(3).await;
-        return outcome_one + outcome_two;
+    let async_outcome = async {
+        // 1. Define futures
+        let mut futures_vec = Vec::new();
+        let outcome_one = do_something(2);
+        let outcome_two = do_something(3);
+        // 2. Push futures into the vector
+        futures_vec.push(outcome_one);
+        futures_vec.push(outcome_two);
+        // 3. Spawn tasks and collect handles
+        let handles = futures_vec
+            .into_iter()
+            .map(async_std::task::spawn)
+            .collect::<Vec<_>>();
+        // 4. Wait for all tasks to complete
+        let results = join_all(handles).await;
+        return results.into_iter().sum::<i8>();
     };
-    let outcome = block_on(future_three);
+    let outcome = block_on(async_outcome);
     println!("time elapsed: {:?}", now.elapsed());
     println!("Here is the outcome: {}", outcome);
 }
